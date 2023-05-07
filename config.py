30f6aca84e2f318dfa4cf54f3dd42d56cada4b5a
WenJie9365 <99584971+WenJie9365@users.noreply.github.com>
2023-05-07 22:43:41 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cz8rnuahdnd8.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "ngwenjie11-employee"
+customregion = "us-east-1a"
+
